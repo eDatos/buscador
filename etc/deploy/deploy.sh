a76cd18906c005f4eb1593a2cabfdfe9bd49d8bf
acidcar <acidcar@arte-consultores.com>
2017-06-27 14:28:26 +0100

    METAMAC-2624 - Crear tarea para despliegue de todo metamac

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -5,6 +5,12 @@ TRANSFER_PATH=$HOME_PATH/tmp
 DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
 
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
+RESTART=1
+
+if [ "$1" == "--no-restart" ]; then
+    RESTART=0
+fi
+
 
 scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
 scp search-web/target/buscador*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/search.war
@@ -12,27 +18,30 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
 
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     . $TRANSFER_PATH/deploy/utilities.sh
-    
-    sudo service metamac01 stop
-    checkPROC "metamac"
-    
+
+    if [ $RESTART -eq 1 ]; then
+        sudo service metamac01 stop
+        checkPROC "metamac"
+    fi
+
     ###
     # BUSCADOR
     ###
-    
+
     # Update Process
     sudo rm -rf $DEPLOY_TARGET_PATH/search
     sudo mv $TRANSFER_PATH/search.war $DEPLOY_TARGET_PATH/search.war
     sudo unzip $DEPLOY_TARGET_PATH/search.war -d $DEPLOY_TARGET_PATH/search
     sudo rm -rf $DEPLOY_TARGET_PATH/search.war
-    
+
     # Restore Configuration
     sudo cp $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/search/WEB-INF/classes/buscador/environment.xml
 
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R metamac.metamac /servers/metamac
+        sudo service metamac01 start
+    fi
 
-
-    sudo chown -R metamac.metamac /servers/metamac
-    sudo service metamac01 start
 #    checkURL "http://estadisticas.arte-consultores.com/search/" "metamac01"
 
 EOF
